22547cb9e6ab5501140e28bd03e4d7355f245752
Glowman554 <55138266+Glowman554@users.noreply.github.com>
2022-09-29 10:24:03 +0000

    reorder amogus.h

--- amogus.h
+++ amogus.h
@@ -2,35 +2,37 @@
 
 #define amogus {
 #define sugoma }
-#define be ==
-#define bussin true
-#define And &&
 
 #define notbe !=
 #define morechungus <=
 #define lesschungus >=
+#define be ==
+#define And &&
 
 #define grow +=
 #define shrink -=
 #define merge |=
-
 #define is =
+#define eats =
+
 #define fr ;
 #define onGod ;
+
+#define bussin true
 #define susin false
 #define fillipo false
 #define gay false
 #define straight true
-#define gangster main
-#define af
-#define eats =
 #define cum 1
+
+#define gangster main
 #define strip __attribute__((naked))
 #define chungus __attribute__((packed))
 
 #define casus case
 #define maximus
 
+#define af
 #define get
 #define the
 #define fuck
